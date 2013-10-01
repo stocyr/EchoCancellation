2776828d6aeffd660de8c4b1626de5faf6e264cc
Marcel Baertschi <marcel.baertschi@gmx.ch>
2013-10-01 16:32:56 +0200

    included LMS algo

--- Matlab/EchoCancellation.m
+++ Matlab/EchoCancellation.m
@@ -1,41 +1,48 @@
 %% ECHO Cancellation Project 
 % Parameter
-td = 600;       % Delay Time in [ms]
+td = 200;       % Delay Time in [ms]
 fs = 8000;      % Sampling frequency
 a = 0.4;        % Gain of the Echo Signal
 
 % Load Signal
-[sound, fswav, nbit]= wavread('Lorem_ipsum.wav');
-sig = sound(1:ceil(fswav/fs):end);  % Undersampling
+[sound, fswav, nbit]= wavread('Lorem_ipsum_4000.wav');
+x = sound(round(1:fswav/fs:end));  % Undersampling
 clearvars sound;
 
 % Create Echo
 nshift = floor(td*10^-3*fs)
-echo = [zeros(nshift,1)' a*sig']';
-d = [sig' zeros(nshift,1)']';
-x = d + echo;
+
+echo = [zeros(nshift,1)' a*x']';
+x = [x' zeros(nshift,1)']';
+y = x + echo;
+
 
 % Play signal
-% soundsc(x, fs)
-% plot(1:length(x), x)
+%  soundsc(y, fs)
+% plot(1:length(y), y)
 
 %% Signal Processing
-% NFIR = 200;
-% y = zeros(NFIR,1);
-% e = ones(NFIR,1);
-% u = ones(NFIR,1);
-% w = ones(NFIR,1);
-% 
-% % Estimation for adaptive FIR-Filter
-% for n = 1:length(x)
-%     y(n) = w(1)'*x(n);
-%     e(n) = d(n)-y(n);
-%    % u(n)=1/(x(n)'*x(n));
-%    w = [w(2)+2*u(1)*e(n)*x(n) w(1:end-1)']';
-% end
+NFIR = 2000;
+w = zeros(NFIR, 1);         % soooo nicht auf ARM, 2 Spalten
+u = 0.01;                   % konvergenzgeschwindigkeit
+x = [zeros(NFIR,1)' x']';
+y = [zeros(NFIR,1)' y']';
 
-plot(y)
-soundsc(y,fs)
+for k = 1:length(x)-NFIR;
+w = w + u*x(k:k+NFIR-1)*(y(k+NFIR)-w'*x(k:k+NFIR-1));
+end
+
+w  = w(end:-1:1);
+rb_=roots(flipud(w));
 
 
+ra = rb_(find(abs(rb_) <=1 ));
+ra = 1./ra;
+rb = rb_(find(abs(rb_) > 1));
+x_filter = filter(ra, rb, x);
+soundsc(100*real(x_filter), 8000);
 
+% R_xx = xcorr(x,x);
+% plot(R_xx)
+% plot(y)
+% soundsc(y,fs)
